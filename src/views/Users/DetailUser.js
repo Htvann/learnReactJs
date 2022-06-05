36db5611b200d9f38f98cf2a9241ce1f22ea83c9
Hồ Thanh Văn <vean@MacBook-Pro-cua-Ho.local>
2022-06-05 17:04:51 +0700

    useNavigate

--- src/views/Users/DetailUser.js
+++ src/views/Users/DetailUser.js
@@ -1,19 +1,42 @@
 import React, { Component } from "react";
-import { useParams } from "react-router-dom";
+import {useNavigate, useParams} from "react-router-dom";
 import axios from "axios";
 
 class DetailUser extends Component {
-    async componentDidMount() {
-        if (this.props.params) {
-            let id = this.props.params.id;
-            let res = await axios.get(`https://reqres.in/api/users/${id}`);
-            console.log(res.data);
-        }
+    state = {
+        user: {}
     }
 
+    async componentDidMount() {
+        // if (this.props.params) {
+            let id = this.props.params.id;
+            let res = await axios.get(`https://reqres.in/api/users/${id}`);
+            this.setState({
+                user: res && res.data && res.data.data ? res.data.data : {}
+            })
+        // }
+    }
+    handleBackButton = () =>{
+        let navigate = this.props.navigation;
+        navigate(`/user/`)
+    }
+
+
     render() {
-        return <div>hello from detail user</div>;
+        let {user} = this.state;
+        return(
+            <>
+                <div>User's name: {user.first_name} - {user.last_name}</div>
+                <div>User's email: {user.email}</div>
+                <div>
+                    <img src={user.avatar} />
+                </div>
+                <div>
+                    <button type="button" onClick={() => this.handleBackButton()}>Back</button>
+                </div>
+            </>
+        )
     }
 }
 
-export default (props) => <DetailUser {...props} params={useParams()} />;
+export default (props) => <DetailUser {...props} params={useParams()} navigation={useNavigate()}/>;
--- src/views/Users/ListUser.js
+++ src/views/Users/ListUser.js
@@ -1,5 +1,6 @@
 import React from "react";
 import axios from "axios";
+import { useNavigate } from "react-router-dom"
 
 class ListUser extends React.Component {
     state = {
@@ -7,11 +8,16 @@ class ListUser extends React.Component {
     };
 
     async componentDidMount() {
-        let res = await axios.get("https://reqres.in/api/users?page=2");
+        let res = await axios.get("https://reqres.in/api/users?page=1");
         this.setState({
             listUsers: res && res.data && res.data.data ? res.data.data : [],
         });
         console.log("check res", res.data.data);
+    }
+    handleShowDetail = (user) =>{
+        console.log("check user: ", user.id)
+        let navigation = this.props.navigation;
+        navigation(`/user/${user.id}`);
     }
 
     render() {
@@ -23,7 +29,7 @@ class ListUser extends React.Component {
                     {
                         listUsers && listUsers.length > 0 && listUsers.map((item, index) => {
                             return (
-                                <div className="child" key={item.id}>
+                                <div className="child" key={item.id} onClick={()=>this.handleShowDetail(item)}>
                                     {index + 1} - {item.first_name}
                                     {item.last_name}
                                 </div>
@@ -36,4 +42,4 @@ class ListUser extends React.Component {
     }
 }
 
-export default ListUser;
+export default (props) => <ListUser {...props} navigation={useNavigate()} />
